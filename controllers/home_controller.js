c067dba77364860536f3765fa2caa89d47a68d76
akshatk806 <akshatk806@gmail.com>
2022-08-13 21:35:25 +0530

    Setting up home.ejs in home_controller

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home=function(request,respone){
-    respone.end('<h1>Express is up for iConnect</h1>')
+    // respone.end('<h1>Express is up for iConnect</h1>');   // this line will sent directly to the browser
+    respone.render('home',{
+        title:"Home"
+    })
 }
