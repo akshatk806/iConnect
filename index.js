bece10609fb4719ebaa62306dc4b98a81036d25f
akshatk806 <akshatk806@gmail.com>
2022-08-13 21:27:49 +0530

    Setting up the view engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app=express();
 // use express router (from routes/index.js)
 app.use('/',require('./routes/index.js'));
 
+// set up the view engine
+app.set('view engine','ejs');
+app.set('views','./views')
+
 app.listen(port,(err)=>{
     if(err){
         console.log("Error occured in firing up the express server:",err);
