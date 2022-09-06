fb32b5ab5942d01de67f2922d2668ea83f9e6a8e
akshatk806 <akshatk806@gmail.com>
2022-09-06 05:47:13 +0530

    Restricting user to post who is not signed in

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,9 @@
 const express=require('express');
 const router=express.Router();
+const passport=require('passport');
 
 const posts_controller=require('../controllers/posts_controller.js');
 
-router.post('/create',posts_controller.create);
+router.post('/create',passport.checkAuthentication,posts_controller.create);
 
 module.exports=router
