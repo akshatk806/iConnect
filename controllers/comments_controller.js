55e392140e16a76f4856633a76d786a9aa2787a5
akshatk806 <akshatk806@gmail.com>
2022-09-06 11:18:24 +0530

    Deleting the comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -17,3 +17,20 @@ module.exports.create=function(request,response){
         }
     })
 }
+
+module.exports.delete=function(request,response){
+    Comment.findById(request.params.id,function(err,comment){
+        if(comment.user==request.user.id){
+            let postId=comment.post
+
+            comment.remove()
+
+            Post.findByIdAndUpdate(postId,{$pull:{comments:request.params.id}},function(err,post){
+                return response.redirect('back');
+            })            // $pull is inbuilt {pull from where, what do I need to pull}
+        }
+        else{
+            return response.redirect('back');
+        }
+    })
+}
--- routes/comments.js
+++ routes/comments.js
@@ -6,4 +6,6 @@ const comments_controller=require('../controllers/comments_controller');
 
 router.post('/create',passport.checkAuthentication,comments_controller.create);
 
+router.get('/delete/:id',passport.checkAuthentication,comments_controller.delete)
+
 module.exports=router
