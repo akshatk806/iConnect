b1ba82cdb30192d5a178c971c8a822a0b2086bf3
akshatk806 <akshatk806@gmail.com>
2022-09-06 06:03:17 +0530

    Creating schema for the comments

--- models/posts.js
+++ models/posts.js
@@ -8,7 +8,15 @@ const postSchema=new mongoose.Schema({
     user:{
         type:mongoose.Schema.Types.ObjectId,             // the post which is done by the user is refering to user schema
         ref:'User'
-    }
+    },
+
+    // Include the array of ids of all comment in this post schema itself
+    comments:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'comment'
+        }
+    ]
 },{
     timestamps:true
 });
