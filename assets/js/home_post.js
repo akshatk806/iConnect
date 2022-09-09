9f2732a5868cc7cf60724a2819a44816ffa251ab
akshatk806 <akshatk806@gmail.com>
2022-09-10 05:07:18 +0530

    Creating a method to create a post and showing it on the DOM

--- assets/js/home_post.js
+++ assets/js/home_post.js
@@ -12,7 +12,8 @@
                 url:'/posts/create',
                 data:newPostForm.serialize(),
                 success:function(data){
-                    console.log(data)
+                    let newPost=newPostDOM(data.data.post)
+                    $('#posts-list-container>ul').prepend(newPost)
                 },
                 error:function(error){
                     console.log(error.responseText)
@@ -21,6 +22,38 @@
         })
     }
 
-    // Method to create a post in DOM
+    // Method to create a post and showing it on the DOM
+    let newPostDOM=function(post){
+        return $(`<li id="post-${post._id}">               
+                    <p>
+                            <small>
+                                <a class="delete-post-button" href="/posts/delete/${post.id}">x</a>
+                            </small>
+                        ${post.content}
+                        <br>
+                        <small style="color: blue;">
+                            <strong>${post.user.name}</strong> 
+                        </small>
+                    </p>
+                
+                    <div class="post-comments">
+                
+                            <form action="/comments/create" method="POST">
+                                <input type="text" name="content" placeholder="Add a comment...">
+                                <input type="hidden" name="post" value="${post._id}">
+                                <input type="submit" value="Add comment">
+                            </form>
+                        
+                        <div class="post-comments-list">
+                            <ul id="post-comments-${post._id}">
+        
+                            </ul>
+                        </div>
+                        
+                    </div>
+                </li>`)
+    }
 
+
+    createPost();
 }
