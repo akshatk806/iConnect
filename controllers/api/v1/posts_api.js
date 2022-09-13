766e5de3fa168db42f9d0b2baef1a6cc0468b841
akshatk806 <akshatk806@gmail.com>
2022-09-13 16:26:40 +0530

    Accessing and deleting a post using API

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,46 @@
-module.exports.index=function(request,response){
+const Post=require('../../../models/posts')
+const Comment=require('../../../models/comment')
+
+module.exports.index=async function(request,response){
+    
+    let posts=await Post.find({}).populate('user')
+    .sort('-createdAt')
+    .populate({
+        path:'comments',
+        populate:{
+            path:'user'
+        }
+    });
+    
     return response.json(200,{
         message:"Lists of posts",
-        posts:[]
+        posts:posts
     });
+}
+
+
+module.exports.delete=async function(request,response){
+
+    try{
+        let post=await Post.findById(request.params.id);
+    
+        // if(post.user == request.user.id){
+            post.remove();
+            
+            await Comment.deleteMany({post:request.params.id})
+
+            return response.json(200,{
+                message:"Post and associated comment delete successfully"
+            });
+        // }
+        // else{
+        //     request.flash('error','You cannot delete this post');
+        //     return response.redirect('back')
+        // }
+    }catch(err){
+        console.log(err);
+        response.json(500,{
+            message:"Internal server error"
+        })
+    }
 }
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -3,6 +3,8 @@ const express=require('express');
 const router=express.Router();
 const postsAPI=require('../../../controllers/api/v1/posts_api');
 
-router.get('/',postsAPI.index);
+router.get('/',postsAPI.index);      // return json back to the browser
+
+router.delete('/:id',postsAPI.delete);
 
 module.exports=router
