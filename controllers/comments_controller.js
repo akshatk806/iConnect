7a10c2e6a3d2ed0f3e0b58fb9d064fd4f1ecf7c6
akshatk806 <akshatk806@gmail.com>
2022-09-17 13:23:44 +0530

    Adding noty to comment activities

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -3,38 +3,50 @@ const Post=require('../models/posts')
 const commentsMailer=require('../mailers/comments_mailer');
 
 module.exports.create=async function(request,response){
-    let post=await Post.findById(request.body.post);
-    if(post){
-        let comment=await Comment.create({
-            content:request.body.content,
-            post:request.body.post,
-            user:request.user._id
-        });
-        post.comments.push(comment);
-        post.save();
-
-        comment=await Comment.findOne({user:request.user._id}).populate('user','name email').exec();
-        
-        // console.log("Comments in comment_controller:",comment);
-        commentsMailer.newComment(comment);
-
-        response.redirect('/');
+    try{
+        let post=await Post.findById(request.body.post);
+        if(post){
+            let comment=await Comment.create({
+                content:request.body.content,
+                post:request.body.post,
+                user:request.user._id
+            });
+            post.comments.push(comment);
+            post.save();
+    
+            comment=await Comment.findOne({user:request.user._id}).populate('user','name email').exec();
+            
+            // console.log("Comments in comment_controller:",comment);
+            commentsMailer.newComment(comment);
+    
+            request.flash('success','Comment Published!');
+            response.redirect('/');
+        }
+    }catch(err){
+        request.flash('error',err)
+        return
     }
 }
 
-module.exports.delete=function(request,response){
-    Comment.findById(request.params.id,function(err,comment){
+module.exports.delete=async function(request,response){
+    try {
+        let comment=await Comment.findById(request.params.id);
         if(comment.user==request.user.id){
             let postId=comment.post
 
             comment.remove()
 
-            Post.findByIdAndUpdate(postId,{$pull:{comments:request.params.id}},function(err,post){
-                return response.redirect('back');
-            })            // $pull is inbuilt {pull from where, what do I need to pull}
-        }
-        else{
+            let post=Post.findByIdAndUpdate(postId,{$pull:{comments:request.params.id}});            // $pull is inbuilt {pull from where, what do I need to pull}
+
+            request.flash('success','Comment deleted!');
             return response.redirect('back');
         }
-    })
+        else{
+            request.flash('error','Unauthorized!');
+            return response.redirect('back');
+        }
+    } catch (error) {
+        request.flash('error',err)
+        return
+    }
 }
