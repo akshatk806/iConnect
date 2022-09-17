8dd20d5256e40e461ee76ac6787efc0c89b0a62d
akshatk806 <akshatk806@gmail.com>
2022-09-17 13:42:11 +0530

    Sent an email using HTML template

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -3,12 +3,13 @@ const nodeMailer=require('../config/nodemailer');
 // this is another way of exporting a method
 exports.newComment=(comment)=>{
 
-    console.log("Inside newComment:",comment);
+    // console.log("Inside newComment:",comment);
+    let htmlString=nodeMailer.renderTemplate({comment:comment},'/comments/new_comment.ejs')
     nodeMailer.transporter.sendMail({
         from:"iConnect.com",
         to:comment.user.email,
         subject:"New Comment published",
-        html:"<h1>Yup, your comment is now published!</h1>"
+        html:htmlString
     },(err,info)=>{
         if(err) {console.log("Error in sending mail",err); return}
 
