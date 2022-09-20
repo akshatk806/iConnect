52cebe5a46e0d4cb930ee6d6b30459b0b19f7531
akshatk806 <akshatk806@gmail.com>
2022-09-20 23:25:06 +0530

    Setting up the production environment and adding the keys of process.env in environment variables

--- config/environment.js
+++ config/environment.js
@@ -20,7 +20,24 @@ const development={
 }
 
 const production={
-    name:'production'
+    name:'production',
+    asset_path:process.env.ICONNECT_ASSET_PATH,
+    session_cookie_key:process.env.ICONNECT_SESSION_COOKIE_KEY,
+    db:process.env.ICONNECT_DB,
+    smtp:{
+            service:'gmail',
+            host:'smtp.gmail.com',
+            port:587,                     // port 587-for TLS and port 465-for SSL
+            secure:false,
+            auth:{
+                user:process.env.ICONNECT_GMAIL_USERNAME,
+                pass:process.env.ICONNECT_GMAIL_PASSWORD
+            }
+    },
+    google_client_id:process.env.ICONNECT_GOOGLE_CLIENT_ID,
+    google_client_secret:process.env.ICONNECT_GOOGLE_CLIENT_SECRET,
+    google_callback_URL:process.env.ICONNECT_GOOGLE_CALLBACK_URL,
+    jwt_secret:process.env.ICONNECT_JWT_SECRET
 }
 
-module.exports=development;
+module.exports=eval(process.env.ICONNECT_ENVIRONMENT)==undefined?development:eval(process.env.ICONNECT_ENVIRONMENT);
